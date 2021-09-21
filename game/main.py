028c5db8dc8067fc147e952a69914066d175b7cf
Karyon Trotters <71095068+RoundingExplorer@users.noreply.github.com>
2021-09-21 21:16:17 +0530

    Update main.py

--- game/main.py
+++ game/main.py
@@ -1,1 +1,10 @@
+import random
+import os
 
+no_of_games= input("How many games do you want to play?: ")
+
+  
+ while no_of_games != int:
+  no_of_games= input("How many games do you want to play? Please put only inte: ")
+  
+                   
